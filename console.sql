6ca1ee7c22b395bb253051d5ce248cb188a96173
Nabil170718 <69347393+Nabil170718@users.noreply.github.com>
2020-09-17 14:00:23 +0200

    Create console.sql

---
+++ console.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : jeu. 17 sep. 2020 à 11:52
+-- Version du serveur :  5.7.24
+-- Version de PHP : 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `jeux_video`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `console`
+--
+-- Erreur de lecture de structure pour la table jeux_video.console : #1146 - La table 'jeux_video.console' n'existe pas
+-- Erreur de lecture des données pour la table jeux_video.console : #1064 - Erreur de syntaxe près de 'FROM `jeux_video`.`console`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
